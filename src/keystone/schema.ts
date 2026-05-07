f15c0a9084ab58753f1657c7c9ebe5f903b9db35
Exolan <evjekddd@mail.ru>
2026-05-07 15:36:02 +0300

    feat: add basic entities

--- src/keystone/schema.ts
+++ src/keystone/schema.ts
@@ -1,10 +1,8 @@
 import { list } from '@keystone-6/core'
 import { allowAll } from '@keystone-6/core/access'
 
-import {
-  text,
-  password
-} from '@keystone-6/core/fields'
+import { text, password, relationship, timestamp, select, integer } from '@keystone-6/core/fields'
+import { release } from 'os'
 
 export const lists = {
   User: list({
@@ -24,7 +22,58 @@ export const lists = {
 
       password: password({
         validation: { isRequired: true }
+      }),
+
+      reviews: relationship({
+        ref: 'Review.user',
+        many: true
       })
     }
+  }),
+
+  Author: list({
+    access: allowAll,
+
+    fields: {
+      firstName: text({ validation: { isRequired: true } }),
+      lastName: text({ validation: { isRequired: true } }),
+      books: relationship({ ref: 'Book.author', many: true })
+    }
+  }),
+
+  Genre: list({
+    access: allowAll,
+
+    fields: {
+      name: text({ validation: { isRequired: true } }),
+      books: relationship({ ref: 'Book.genre', many: true })
+    }
+  }),
+
+  Book: list({
+    access: allowAll,
+
+    fields: {
+      title: text({ validation: { isRequired: true } }),
+      author: relationship({ ref: 'Author.books', many: false }),
+      genre: relationship({ ref: 'Genre.books', many: true }),
+      reviews: relationship({ ref: 'Review.book', many: true })
+    }
+  }),
+
+  Review: list({
+    access: allowAll, // ИЗМЕНИТЬ, ГОСТИ НЕ МОГУТ ВИДЕТЬ
+
+    fields: {
+      user: relationship({ ref: 'User.reviews', many: false }),
+      book: relationship({ ref: 'Book.reviews', many: false }),
+      text: text(),
+      createdAt: timestamp(),
+      score: integer({
+        validation: { isRequired: true, min: 1, max: 5 }
+      })
+    }
+
+    // НУЖНА ПРОВЕРКА НА ТО, ЧТО У ПОЛЬЗОВАТЕЛЯ МОЖЕТ БЫТЬ ТОЛЬКО ОДИН ОТЗЫВ НА ЭТУ КНИГУ
   })
 }
