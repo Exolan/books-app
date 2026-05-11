92f70962d858a916b9eb1aa85d583c0a9ba50ffe
Exolan <evjekddd@mail.ru>
2026-05-11 09:33:51 +0300

    fix: add message error to book list

--- src/widgets/list/ui/BooksList.tsx
+++ src/widgets/list/ui/BooksList.tsx
@@ -2,10 +2,15 @@ import { useBooks } from 'src/entities/book'
 import { BookCard } from 'src/entities/book'
 
 export default function BooksList() {
-  const { books, loading } = useBooks()
+  const { books, loading, error } = useBooks()
 
   if (loading) {
     return <p>Загрузка книг...</p>
+  }
+
+  if (error) {
+    console.error('Ошибка загрузки книг:', error)
+    return <p>Не удалось загрузить книги. Попробуйте позже</p>
   }
 
   if (!books?.length) {
